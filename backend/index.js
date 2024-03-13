761da68a80302fc3f13a26ccb6dedb4f6d5bc18a
jekjeki <yusufzaki013@gmail.com>
2024-03-13 12:08:25 +0700

    handle-cirs

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const port = 8080
 const customerRouter = require('./routes/customerRoute')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use(
     express.urlencoded({
