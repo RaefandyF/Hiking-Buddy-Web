d6b55c99cd7dc81ed551099f070debe4d5c28de4
jekjeki <yusufzaki013@gmail.com>
2024-04-15 12:17:38 +0700

    fix-card-route

--- pages/route/index.js
+++ pages/route/index.js
@@ -1,5 +1,8 @@
 import NavbarComponent from '@/components/Navbar'
 import React from 'react'
+import { IoLocationSharp } from "react-icons/io5";
+import { MdAccessTimeFilled } from "react-icons/md";
+import { FaStar } from "react-icons/fa6";
 
 function index() {
   return (
@@ -38,9 +41,39 @@ function index() {
             <div className='h-[320px] min-w-[250px] relative mt-[5px] mx-3'>
                 <div className='absolute w-full h-full'>
                     <img src='/route_card_image.png' className='w-full h-full' />
-                    <div className='flex absolute top-0 m-3 mx-5'>
+                    <div className='w-[200px] flex justify-between absolute top-3 m-3 mx-5'>
                         <div className='text-white text-center rounded-lg w-[80px] backdrop-blur-sm bg-white/30'>
                             <p>Lombok</p>
+                        </div>
+                        <div>
+                            <img src='/save_icon.png' />
+                        </div>
+                    </div>
+                    <div className='absolute rounded-[20px] py-2 bg-black bg-opacity-50 translate-x-[10px] text-center text-white bottom-[40px]'>
+                        <div className='w-[230px] h-[100px]'>
+                            <div className='font-bold'>
+                                <p>Gunung Rinjani</p>
+                            </div>
+                            <div className='flex justify-left items-center mx-3'>
+                                <IoLocationSharp />
+                                <div className='mx-1'>
+                                    <p>Jarak 5,5km</p>
+                                </div>
+                            </div>
+                            <div className='flex justify-between items-center mx-3'>
+                                <div className='flex items-center'>
+                                    <MdAccessTimeFilled />
+                                    <div className='mx-1'>
+                                        <p>4 jam</p>
+                                    </div>
+                                </div>
+                                <div className='flex items-center'>
+                                    <FaStar color='#FFC94A' className='mx-2' />
+                                    <div className=''>
+                                        <p>4.9</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
