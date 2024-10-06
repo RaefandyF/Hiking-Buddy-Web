47ea648bb6266b598c0efa287d017e89f25b7fdf
jekjeki <yusufzaki013@gmail.com>
2024-10-06 22:41:09 +0700

    add-new-frontend-data

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,9 @@
 const config = {
     db: {
+        // vm
+        // 34.27.70.191
+        // mysql
+        // 35.222.84.198
         host: 'localhost', 
         user: 'root', 
         password: '', 
